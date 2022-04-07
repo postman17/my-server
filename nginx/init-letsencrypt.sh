7541ee8ce198c84de635cba8b189fc072cb34f73
postman17 <frompostal@gmail.com>
2022-04-07 18:30:52 +0500

    eleventh attempt

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -7,7 +7,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(cloud.postman17.tech www.cloud.postman17.tech pgadmin.postman17.tech www.pgadmin.postman17.tech)
+domains="cloud.postman17.tech pgadmin.postman17.tech"
 rsa_key_size=4096
 data_path="./data/certbot"
 docker_compose_file_path="nginx/docker-compose.yml"
@@ -31,15 +31,16 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
 fi
 
 echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
-docker-compose -f $docker_compose_file_path run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
-echo
-
+for domain in $domains; do
+  path="/etc/letsencrypt/live/$domain"
+  mkdir -p "$data_path/conf/live/$domain"
+  docker-compose -f $docker_compose_file_path run --rm --entrypoint "\
+    openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+      -keyout '$path/privkey.pem' \
+      -out '$path/fullchain.pem' \
+      -subj '/CN=localhost'" certbot
+  echo
+done
 
 echo "### Starting nginx ..."
 docker-compose -f $docker_compose_file_path up --force-recreate -d nginx
@@ -47,50 +48,48 @@ echo
 
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose -f $docker_compose_file_path run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
+for domain in $domains; do
+  docker-compose -f $docker_compose_file_path run --rm --entrypoint "\
+    rm -Rf /etc/letsencrypt/live/$domain && \
+    rm -Rf /etc/letsencrypt/archive/$domain && \
+    rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
+  echo
+done
 
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
+
+for domain in $domains; do
+  #Join $domain to -d args
+  domain_args="-d $domain -d 'www.$domain'"
+
+  # Select appropriate email arg
+  case "$email" in
+    "") email_arg="--register-unsafely-without-email" ;;
+    *) email_arg="--email $email" ;;
+  esac
+
+  # Enable staging mode if needed
+  if [ $staging != "0" ]; then staging_arg="--staging"; fi
+
+  docker-compose -f $docker_compose_file_path run --rm --entrypoint "\
+    certbot certonly --webroot -w /var/www/certbot \
+      $staging_arg \
+      $email_arg \
+      $domain_args \
+      --rsa-key-size $rsa_key_size \
+      --agree-tos \
+      --force-renewal" certbot
+  echo
 done
 
-# Select appropriate email arg
-case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
-esac
 
-# Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
-
-docker-compose -f $docker_compose_file_path run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
-    $staging_arg \
-    $email_arg \
-    $domain_args \
-    --rsa-key-size $rsa_key_size \
-    --agree-tos \
-    --force-renewal" certbot
-echo
-
-
-echo "### Enabled domains with SSL ..."
+echo "### Enabled domains in nginx with SSL ..."
 for domain in $domains; do
-  if [[ "$domain" != *"www"* ]]; then
-    echo "now enabling this domain: $domain"
-    docker-compose -f $docker_compose_file_path exec nginx mv -v "/etc/nginx/conf.d/ssl.$domain.conf.disabled" "/etc/nginx/conf.d/ssl.$domain.conf"
-    echo "now disabling this domain: $domain"
-    docker-compose -f $docker_compose_file_path exec nginx mv -v "/etc/nginx/conf.d/$domain.conf" "/etc/nginx/conf.d/$domain.conf.disabled"
-  else
-    echo "skipped: $domain"
-  fi
+  echo "now enabling this domain: $domain"
+  docker-compose -f $docker_compose_file_path exec nginx mv -v "/etc/nginx/conf.d/ssl.$domain.conf.disabled" "/etc/nginx/conf.d/ssl.$domain.conf"
+  echo "now disabling this domain: $domain"
+  docker-compose -f $docker_compose_file_path exec nginx mv -v "/etc/nginx/conf.d/$domain.conf" "/etc/nginx/conf.d/$domain.conf.disabled"
 done
 
 

f38151bdcc05173bbe18fd952b2ec9f9dc5e7148
postman17 <frompostal@gmail.com>
2022-04-07 20:00:41 +0500

    fifteenth attempt

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -12,7 +12,7 @@ rsa_key_size=4096
 data_path="./data/certbot"
 docker_compose_file_path="nginx/docker-compose.yml"
 email="frompostal@yandex.ru" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
